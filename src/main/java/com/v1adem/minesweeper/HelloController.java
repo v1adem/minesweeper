fb1b7b448db85dc8b47aa55396b9f9463776c705
v1adem <v1adem@ukr.net>
2023-07-03 15:50:08 +0300

    Initial commit

---
+++ src/main/java/com/v1adem/minesweeper/HelloController.java
@@ -0,0 +1,14 @@
+package com.v1adem.minesweeper;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
